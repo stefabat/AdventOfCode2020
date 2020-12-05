e791dbff8614c83c380ed138e1f1a79afc453510
Stefano Battaglia <battaglia1803@gmail.com>
2020-12-05 12:31:23 +0100

    Part two of day 2 solved

--- day02/day02.jl
+++ day02/day02.jl
@@ -19,8 +19,7 @@ passwords = readdlm("input.txt",' ',String)
 valid = 0
 
 for i = 1:size(passwords,1)
-  # first column contains elements like "x-y" where x and y are integers
-  # with x < y
+  # first column contains elements like "x-y" where x and y are integers with x < y
   minmax = split(passwords[i,1],'-')
   min = parse(Int,minmax[1])
   max = parse(Int,minmax[2])
@@ -38,4 +37,39 @@ for i = 1:size(passwords,1)
 end
 
 println("There are $valid valid passwords out of $(size(passwords,1))")
+
+
+#--- Part two ---#
+println("#--- Part two ---#")
+
+# In part two, the policy is actually difference. What previously were min
+# and max, they are now exact positions in the password. At exclusively one
+# of these positions, the character of the policy has to appear. If it appears
+# twice or zero times, the password is not valid.
+
+# initialize counter of valid passwords
+valid = 0
+
+for i = 1:size(passwords,1)
+  # first column contains elements like "x-y" where x and y are integers with x < y
+  positions = split(passwords[i,1],'-')
+  i₁ = parse(Int,positions[1])
+  i₂ = parse(Int,positions[2])
+
+  # second column contains elements like "c:" where c is a character
+  # the only function is used to make a string of one character to Char
+  character = only(chop(passwords[i,2], tail = 1))
+
+  # third column contains the passwords
+  occurences = count(==(character),[passwords[i,3][i₁],passwords[i,3][i₂]])
+
+  if occurences == 1
+    global valid += 1
+  end
+end
+
+println("There are $valid valid passwords out of $(size(passwords,1))")
+
+
+
 
