992669339d99309a73a2b8e35f1170f08287a336
Stefano Battaglia <battaglia1803@gmail.com>
2020-12-05 17:32:56 +0100

    Part 2 of day 3 done

--- day03/day03.jl
+++ day03/day03.jl
@@ -15,8 +15,8 @@ N = length(slope)
 M = length(slope[1])
 
 # set the movements
-right = 3
 down  = 1
+right = 3
 
 # indices to access the map
 i = 1
@@ -35,5 +35,44 @@ while i < N
 end
 
 println("Total number of trees encountered: $trees")
+
+#--- Part two ---#
+
+println("#--- Part two ---#")
+
+# set up an array containing the movements as namedtuples
+movements = [(down = 1, right = 1),
+             (down = 1, right = 3),
+             (down = 1, right = 5),
+             (down = 1, right = 7),
+             (down = 2, right = 1)]
+
+# counter of trees for each movement combination
+trees = zeros(Int, length(movements))
+
+for (k,movement) in enumerate(movements)
+  global down  = movement.down
+  global right = movement.right
+
+  # indices to access the map
+  global i = 1
+  global j = 0 # starting at zero because we use the modulo
+
+  while i < N
+    i += down
+    j  = (j + right)%M
+
+    if slope[i][j+1] == '#'
+      global trees[k] += 1
+    end
+  end
+
+  println("Total number of trees encountered with down $down, right $right: $(trees[k])")
+end
+
+println("Product of all trees encountered: $(prod(trees))")
+
+
+
 
 
