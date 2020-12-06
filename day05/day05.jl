cd14fb1b4c9c35b8ab617f520aea5f8a6f4f8bda
Stefano Battaglia <battaglia1803@gmail.com>
2020-12-06 16:39:15 +0100

    day 5 part 2 done

--- day05/day05.jl
+++ day05/day05.jl
@@ -38,3 +38,32 @@ end
 
 println("The highest ID is $maxID")
 
+#--- Second Part ---#
+println("#--- Second Part ---#")
+
+# an empty seat is a zero, an occupied seat is 1. The running index labels
+# the boarding pass ID
+seats = zeros(Int,128*8)
+
+# go through the boarding process and for each pass, set the corresponding seat to 1
+# the + 1 is necessary because in principle there might be a seat 0
+for pass in passes
+  global seats[getID(getrow(pass[1:7]), getcolumn(pass[8:10])) + 1] = 1
+end
+
+# find where the ids for this flight start
+first_id = findfirst(==(1), seats)
+last_id  = findlast(==(1), seats)
+
+# according to the description of the problem, there should be only one zero (empty)
+# seat left between the first_id and last_id, so we look for it and add the padding
+# from the first ids
+myid  = findall(==(0),seats[first_id:last_id])[1]
+myid += first_id - 1
+
+println("The ID of my seat is $(myid - 1)")
+
+
+
+
+
